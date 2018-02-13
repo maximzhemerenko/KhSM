728a0ea3d03808743a8696779eeb84d44e5762f2
Maxim Zhemerenko <maxim.zhemerenko@outlook.com>
2018-02-13 20:50:01 +0200

    Backend project created

---
+++ Backend/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
