7ab91f755e619d9aab2bd6eec6cec068967a469a
marym <marymazel@outlook.com>
2018-02-19 19:28:56 +0200

    Обновила схему данных, добавила скрипт создания бд

--- Database/Initialize.sql
+++ Database/Initialize.sql
@@ -9,6 +9,90 @@ grant select, insert, update, delete on kh_sm.* TO 'kh_sm'@'localhost';
 
 create table User
 (
-    user_id int primary key auto_increment,
-    first_name varchar(32) not null
+    user_id int primary key auto_increment not null,
+    first_name varchar(32) not null,
+    last_name varchar(32) not null,
+    city varchar(32) not null,
+    wca_id varchar(16) not null,
+    phone_number varchar(16) not null,
+    gender varchar(8) not null,
+    birth_date date null,
+    approved date null
+);
+
+create table Meeting
+(
+	meeting_id int primary key auto_increment not null,
+    meeting_number int not null,
+	`date` date not null
+);
+
+create table News
+(
+	news_id int primary key auto_increment not null,
+	user_id int not null,
+    `text` text not null,
+    date_and_time datetime not null,
+    foreign key (user_id) references user(user_id)
+);
+
+create table Discipline
+(
+	discipline_id int primary key auto_increment not null,
+    `name` varchar(32) not null,
+    description text null,
+    attempt_count int not null
+);
+
+create table Result
+(
+	result_id int primary key auto_increment not null,
+    average decimal(5, 2) null,
+    meeting_id int not null unique,
+    user_id int not null unique,
+    discipline_id int not null unique,
+    foreign key (meeting_id) references Meeting(meeting_id),
+    foreign key (user_id) references User(user_id),
+    foreign key (discipline_id) references Discipline(discipline_id)
+);
+
+create table Attempt
+(
+	attempt_id int primary key auto_increment not null,
+    result_id int not null,
+    `time` decimal(5, 2) null,
+    foreign key (result_id) references Result(result_id)
+);
+
+create table Role
+(
+	role_id int primary key auto_increment not null,
+    `name` varchar(16) not null unique,
+    role_key varchar(16) not null unique
+);
+
+create table UserRole
+(
+	user_id int primary key auto_increment not null,
+    role_id int not null,
+    foreign key (user_id) references User(user_id),
+    foreign key (role_id) references Role(role_id)
+);
+
+create table Login
+(
+	user_id int primary key auto_increment not null unique,
+    password_hash binary not null,
+    password_salt int not null,
+    disabled date null,
+    foreign key (user_id) references User(user_id)
+);
+
+create table `Session`
+(
+	session_id int primary key auto_increment not null,
+    user_id int not null,
+    session_key binary not null unique,
+    created date not null,
+    foreign key (user_id) references User(user_id)
 );
---
+++ Database/TestData.sql
@@ -0,0 +1,19 @@
+use kh_sm;
+set SQL_SAFE_UPDATES = 0;
+
+delete from meeting;
+
+insert into meeting(meeting_number, `date`) 
+values (281, '2018-2-18');
+
+insert into meeting(meeting_number, `date`) 
+values (280, '2018-2-11');
+
+insert into meeting(meeting_number, `date`) 
+values (279, '2018-2-4');
+
+insert into meeting(meeting_number, `date`) 
+values (278, '2018-1-29');
+
+insert into meeting(meeting_number, `date`) 
+values (277, '2018-1-22');
