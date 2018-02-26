d5ca9cd34876010e1e0fae377d1d03b03fd584cf
marym <marymazel@outlook.com>
2018-02-26 19:02:34 +0200

    Menu created

--- Android/app/src/main/java/com/khsm/app/presentation/ui/screens/MainActivity.java
+++ Android/app/src/main/java/com/khsm/app/presentation/ui/screens/MainActivity.java
@@ -1,13 +1,25 @@
 package com.khsm.app.presentation.ui.screens;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.khsm.app.R;
+import com.khsm.app.presentation.ui.screens.disciplines.DisciplineListFragment;
+import com.khsm.app.presentation.ui.screens.meetings.MeetingListFragment;
 import com.khsm.app.presentation.ui.screens.meetings.MeetingResultsFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +31,42 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.content, MeetingResultsFragment.newInstance(null))
                     .commit();
         }
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        ImageView avatar_imageView = headerView.findViewById(R.id.avatar_imageView);
+        Glide.with(this)
+                .load("http://animals.yakohl.com/pic/schneeeule-2592013.jpg")
+                .apply(RequestOptions.circleCropTransform())
+                .into(avatar_imageView);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+
+                        if (menuItem.getItemId() == R.id.last_meeting)
+                        {
+                            replaceFragment(MeetingResultsFragment.newInstance(null));
+                        }
+                        else if (menuItem.getItemId() == R.id.meetings)
+                        {
+                            replaceFragment(MeetingListFragment.newInstance());
+                        }
+                        else if (menuItem.getItemId() == R.id.disciplines)
+                        {
+                            replaceFragment(DisciplineListFragment.newInstance());
+                        }
+
+                        return true;
+                    }
+                });
     }
 
     public void replaceFragment(Fragment fragment) {
