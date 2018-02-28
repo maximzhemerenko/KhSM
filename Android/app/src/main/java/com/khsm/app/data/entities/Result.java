70d77a43696823753da278a55b6d49b8f3308611
Maxim Zhemerenko <maxim.zhemerenko@outlook.com>
2018-02-28 21:44:05 +0200

    Refactoring

--- Android/app/src/main/java/com/khsm/app/data/entities/Result.java
+++ Android/app/src/main/java/com/khsm/app/data/entities/Result.java
@@ -8,12 +8,14 @@ public class Result {
     public final User user;
     public final Float average;
     public final List<Float> attempts;
+    public final Integer attemptCount;
 
-    public Result(Integer id, Meeting meetings, User user, Float average, List<Float> attempts) {
+    public Result(Integer id, Meeting meetings, User user, Float average, List<Float> attempts, Integer attemptCount) {
         this.id = id;
         this.meetings = meetings;
         this.user = user;
         this.average = average;
         this.attempts = attempts;
+        this.attemptCount = attemptCount;
     }
 }
